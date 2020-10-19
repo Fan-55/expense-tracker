8843daf406f32d92f165f273bb907b0bbbd9ca50
Fan5 <openconvenience@gmail.com>
2020-10-20 03:09:12 +0800

    feat: set up routes

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
 const app = express()
 const PORT = process.env.PORT || 3000
+const routes = require('./routes/index')
 require('./config/mongoose')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(routes)
 
 app.listen(PORT, () => console.log(`This app is listening at ${PORT}`))
