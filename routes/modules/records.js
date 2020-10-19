14f665578c18ab5bb38f35496d8fe2a780c22618
Fan5 <openconvenience@gmail.com>
2020-10-20 06:46:41 +0800

    feat: add create new record function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,4 +1,33 @@
 const express = require('express')
 const router = express.Router()
 
+const Record = require('../../models/record')
+const Category = require('../../models/category')
+
+router.get('/new', (req, res, next) => {
+  Category.find()
+    .select('name')
+    .lean()
+    .then(categories => {
+      const categoryList = categories.map(category => {
+        return {
+          name: category.name,
+          id: category._id
+        }
+      })
+      res.render('new', { categoryList })
+    })
+})
+
+router.post('/', (req, res, next) => {
+  const { name, date, category, amount } = req.body
+  const newRecord = new Record({
+    name, date, category, amount
+  })
+  newRecord.save()
+    .then(result => {
+      res.redirect('/')
+    })
+})
+
 module.exports = router
