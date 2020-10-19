1485ed05a579cccf4841738ec9324dceb8ceaaac
Fan5 <openconvenience@gmail.com>
2020-10-20 06:06:10 +0800

    feat: add GET / route to browse all records in index.hbs

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,4 +1,29 @@
 const express = require('express')
 const router = express.Router()
 
+const Record = require('../../models/record')
+const Category = require('../../models/category')
+
+router.get('/', (req, res, next) => {
+  const getDate = require('../../utils/getDate')
+  Record.find()
+    .select('name date category amount')
+    .populate('category', 'name icon -_id')
+    .lean()
+    .then(results => {
+      const records = results.map(record => {
+        const date = getDate(record.date)
+        return {
+          id: record._id,
+          name: record.name,
+          category: record.category.name,
+          icon: record.category.icon,
+          date: date,
+          amount: record.amount
+        }
+      })
+      res.render('index', { records })
+    })
+})
+
 module.exports = router
---
+++ utils/getDate.js
@@ -0,0 +1,6 @@
+module.exports = (dateObj) => {
+  const year = dateObj.getFullYear()
+  const month = dateObj.getMonth() + 1
+  const date = dateObj.getDate()
+  return `${year}/${month}/${date}`
+}
