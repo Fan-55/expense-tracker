020e075fa26322ca2fcfed9f0f4979c053c24740
Fan5 <openconvenience@gmail.com>
2020-10-20 11:07:24 +0800

    refactor: simplify the home route

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,25 +5,11 @@ const Record = require('../../models/record')
 const Category = require('../../models/category')
 
 router.get('/', (req, res, next) => {
-  const getDate = require('../../utils/getDate')
   Record.find()
-    .select('name date category amount')
     .populate('category', 'name icon -_id')
     .lean()
-    .then(results => {
-      const records = results.map(record => {
-        const date = getDate(record.date)
-        return {
-          id: record._id,
-          name: record.name,
-          category: record.category.name,
-          icon: record.category.icon,
-          date: date,
-          amount: record.amount
-        }
-      })
-      res.render('index', { records })
-    })
+    .then(records => res.render('index', { records }))
+    .catch(err => console.log(err))
 })
 
 module.exports = router
